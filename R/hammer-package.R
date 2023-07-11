857d704b45e200f883f8dbf24a03fadd67d866a0
Michal Burda <michal.burda@osu.cz>
2023-07-11 15:36:05 +0200

    Added contract testers

---
+++ R/hammer-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom cli cli_abort
+#' @importFrom rlang is_integer
+#' @importFrom rlang is_integerish
+#' @importFrom rlang is_scalar_character
+#' @importFrom rlang is_scalar_double
+#' @importFrom rlang is_scalar_integerish
+#' @importFrom rlang is_scalar_logical
+## usethis namespace: end
+NULL
