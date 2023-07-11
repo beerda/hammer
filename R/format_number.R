98d325e52a2081588103eb63561dfa727200a53a
Michal Burda <michal.burda@osu.cz>
2023-07-11 15:42:50 +0200

    Documented format_number()

--- R/format_number.R
+++ R/format_number.R
@@ -1,3 +1,11 @@
+#' Convert the numeric vector to character vector
+#'
+#' @param x the numeric vector to be converted
+#' @param digits the number of decimal point numbers to round the numbers to
+#' @param na the string to be used for `NA` values
+#' @returns a character vector of transformed numeric values
+#'
+#' @author Michal Burda
 #' @export
 format_number <- function(x,
                           digits = 2,
@@ -6,12 +14,12 @@ format_number <- function(x,
   .must_be_integerish_scalar(digits)
   .must_be_greater_eq(digits, 0)
   .must_be_character_scalar(na)
-  
+
   result <- format(round(x, digits = digits),
                    nsmall = digits,
                    scientific = FALSE,
                    trim = TRUE)
   result[is.na(x)] <- na
-  
+
   result
 }
