1ed19b8e9f44e40ac854ec0521426a9304532dda
Michal Burda <michal.burda@centrum.cz>
2023-08-12 05:22:55 +0200

    Fixed aggreg_percent() to return formatted string

--- R/aggreg_percent.R
+++ R/aggreg_percent.R
@@ -6,6 +6,7 @@
 #' The integerish value is treated as the index of level to be counted, character value selects
 #' the level to be counted by its name, and `NA` selects missing values to be counted.
 #' @param use_na if `FALSE` then `NA` values are omitted prior computing the frequency.
+#' @param ... further arguments passed to [format_percent()]
 #' @return The relative number of occurences of `level` in `x`, i.e., the fraction in [0,1].
 #' @seealso [aggreg_count()]
 #' @author Michal Burda
@@ -15,7 +16,10 @@
 #' aggreg_percent(c(F,F,T,T,T), level = "FALSE")                    # 2/5
 #' aggreg_percent(c("b", "a", "a"), level = "a")                    # 2/3
 #' aggreg_percent(c(NA, "b", "a", "a"), level = NA, use_na = TRUE)  # 1/4
-aggreg_percent <- function(x, level = 1, use_na = FALSE) {
+aggreg_percent <- function(x,
+                           level = 1,
+                           use_na = FALSE,
+                           ...) {
     .must_be_atomic_vector(x)
     ..must_be_type(function(a) is_scalar_integerish(a) ||
                        is_scalar_character(a) ||
@@ -29,5 +33,5 @@ aggreg_percent <- function(x, level = 1, use_na = FALSE) {
     p <- aggreg_count(x, level)
     n <- length(x)
 
-    p / n
+    format_percent(p / n, ...)
 }
