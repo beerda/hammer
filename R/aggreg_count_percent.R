1e22d6517f578fe5262a1572e566594ed6ed981a
Michal Burda <michal.burda@centrum.cz>
2023-08-17 01:46:35 +0200

    Added aggreg_count_percent()

---
+++ R/aggreg_count_percent.R
@@ -0,0 +1,33 @@
+#' Compute absolute and relative frequency of the given level in the given vector.
+#'
+#' The function returns the absolute and relative number (percentage) of occurences
+#' of `level` in vector `x`.
+#' @param x the vector to compute the occurences of `level` in. It must be an atomic vector.
+#' @param level the level to be computed. It could be either integerish value or a string or `NA`.
+#' The integerish value is treated as the index of level to be counted, character value selects
+#' the level to be counted by its name, and `NA` selects missing values to be counted.
+#' @param use_na if `FALSE` then `NA` values are omitted prior computing the frequency.
+#' @param ... further arguments passed to [format_count_percent()]
+#' @return The absolute and relative number of occurences of `level` in `x`.
+#' @seealso [format_count_percent()], [aggreg_count()], [aggreg_percent()]
+#' @author Michal Burda
+#' @export
+aggreg_count_percent <- function(x,
+                                 level = 1,
+                                 use_na = FALSE,
+                                 ...) {
+    .must_be_atomic_vector(x)
+    ..must_be_type(function(a) is_scalar_integerish(a) ||
+                       is_scalar_character(a) ||
+                       (length(a) == 1L && is.na(a)),
+                   "integerish or character scalar")(level)
+    .must_be_flag(use_na)
+
+    if (!use_na) {
+        x <- na.omit(x)
+    }
+    p <- aggreg_count(x, level)
+    n <- length(x)
+
+    format_count_percent(p = p, n = n, ...)
+}
