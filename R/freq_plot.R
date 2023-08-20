62f37ddefa187ebb901c30686858fd178d074563
Michal Burda <michal.burda@centrum.cz>
2023-08-20 15:35:05 +0200

    Added freq_plot()

---
+++ R/freq_plot.R
@@ -0,0 +1,32 @@
+#' Plot the frequency bar chart with absolute and relative count in text over each
+#' bar.
+#'
+#' @param x The factor data to plot
+#' @param name the name of the data axis
+#' @export
+#' @author Michal Burda
+freq_plot <- function(x,
+                      name = deparse(substitute(x))) {
+    .must_be_factor(x)
+    .must_be_character_scalar(name)
+
+  levels(x) <- c(levels(x), '(NA)')
+  x[is.na(x)] <- '(NA)'
+
+  d <- data.frame(x = x)
+  lim <- max(table(d$x))
+
+  g <- ggplot(d) +
+      aes(y = x, x = after_stat(count)) +
+      geom_bar() +
+      geom_text(aes(label = after_stat(paste0(count, ' (', round(100 * count / sum(count)), ' %)'))),
+                hjust =  -0.2,
+                stat = 'count',
+                color = "black",
+                size = 3) +
+      scale_x_continuous(expand = c(0, 0),
+                         limits = c(NA, lim * 1.15)) +
+      ylab(name)
+
+  g
+}
