c012aff280614c7dcf8b733dce334d12ff7f32b6
Michal Burda <michal.burda@osu.cz>
2023-08-31 10:04:09 +0200

    Fixed format_bars() test

--- tests/testthat/test-format_bars.R
+++ tests/testthat/test-format_bars.R
@@ -1,24 +1,24 @@
 test_that("format_bars", {
   expect_equal(format_bars(p = 0, n = 10, width = 5),
-               "-----")
+               "     ")
   expect_equal(format_bars(p = 1, n = 10, width = 5),
-               "I----")
+               "I    ")
   expect_equal(format_bars(p = 2, n = 10, width = 5),
-               "I----")
+               "I    ")
   expect_equal(format_bars(p = 3, n = 10, width = 5),
-               "II---")
+               "II   ")
   expect_equal(format_bars(p = 4, n = 10, width = 5),
-               "II---")
+               "II   ")
   expect_equal(format_bars(p = 5, n = 10, width = 5),
-               "II---")
+               "II   ")
   expect_equal(format_bars(p = 6, n = 10, width = 5),
-               "III--")
+               "III  ")
   expect_equal(format_bars(p = 7, n = 10, width = 5),
-               "IIII-")
+               "IIII ")
   expect_equal(format_bars(p = 8, n = 10, width = 5),
-               "IIII-")
+               "IIII ")
   expect_equal(format_bars(p = 9, n = 10, width = 5),
-               "IIII-")
+               "IIII ")
   expect_equal(format_bars(p = 10, n = 10, width = 5),
                "IIIII")
 })
