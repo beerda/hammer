fce8823e7cb73cb3f0a0117a9c9661facd8e55a9
Michal Burda <michal.burda@osu.cz>
2023-08-31 17:26:08 +0200

    histogram() made horizontal

--- R/histogram.R
+++ R/histogram.R
@@ -21,60 +21,58 @@ histogram <- function(x,
     bins <- nclass.Sturges(x)
     binwidth <- (max(x, na.rm = TRUE) - min(x, na.rm = TRUE)) / bins
     g <- ggplot(d) +
-        aes(x = x) +
-        geom_histogram(bins = bins, colour = "black", fill = "white") +
-        geom_density(aes(y = binwidth * after_stat(.data$count)))
+        aes(y = x) +
+        geom_histogram(bins = bins, colour = "black", fill = "gray") +
+        geom_density(aes(x = binwidth * after_stat(.data$count)))
 
     if (nrow(d) > 0) {
         space <- 0.35
         built <- ggplot_build(g)
         xrange <- built$layout$panel_params[[1]]$x.range
         yrange <- built$layout$panel_params[[1]]$y.range
-        yrange[1] <- yrange[1] * 2
-        bcenter <- yrange[1] * ((1 - space) / 2 + space)
+        xrange[1] <- xrange[1] * 2
+        bcenter <- xrange[1] * ((1 - space) / 2 + space)
         g <- g +
-            ylim(yrange[1], yrange[2]) +
-            geom_errorbarh(aes(y = bcenter,
-                               xmin = bp$stats[1],
-                               xmax = bp$stats[5]),
+            xlim(xrange[1], xrange[2]) +
+            geom_errorbar(aes(x = bcenter,
+                               ymin = bp$stats[1],
+                               ymax = bp$stats[5]),
                            color = "black",
-                           height = abs(yrange[1] * (1 - space)),
+                           width = abs(xrange[1] * (1 - space)),
                            size = space) +
-            geom_rect(aes(xmin = bp$stats[2],
-                          xmax = bp$stats[4],
-                          ymin = yrange[1] + space,
-                          ymax = yrange[1] * space),
+            geom_rect(aes(ymin = bp$stats[2],
+                          ymax = bp$stats[4],
+                          xmin = xrange[1] + space,
+                          xmax = xrange[1] * space),
                       color="black",
-                      fill="grey70") +
-            geom_linerange(aes(x = bp$stats[3],
-                               ymin = yrange[1],
-                               ymax = yrange[1] * space))
+                      fill="white") +
+            geom_linerange(aes(y = bp$stats[3],
+                               xmin = xrange[1],
+                               xmax = xrange[1] * space))
 
         if (nrow(outs) > 0) {
             g <- g +
                 geom_point(data = outs,
-                           mapping = aes(x = .data$o, y = bcenter))
+                           mapping = aes(y = .data$o, x = bcenter))
         }
         if (rugs) {
             g <- g +
                 geom_rug(data = d,
-                         mapping = aes(x = x), color = "gray20")
+                         mapping = aes(y = x), color = "gray20")
         }
         if (na) {
             nas <- sum(is.na(x))
-            minx <- min(x, na.rm = TRUE)
-            maxx <- max(x, na.rm = TRUE)
-            minx <- xrange[1]
-            maxx <- xrange[2]
-            diffx <- maxx - minx
+            miny <- yrange[1]
+            maxy <- yrange[2]
+            diffy <- maxy - miny
             g <- g +
-                geom_linerange(ymin = 0,
-                               ymax = nas,
-                               x = maxx + 0.005 * diffx,
+                geom_linerange(xmin = 0,
+                               xmax = nas,
+                               y = maxy + 0.005 * diffy,
                                size = 2) +
-                scale_x_continuous(expand = c(0.1, 0))
+                scale_y_continuous(expand = c(0.1, 0))
         }
     }
 
-    g + xlab(name) + ylab("count")
+    g + xlab("count") + ylab(name)
 }
