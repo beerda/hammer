c1a8faa029f4f8e092b350314eebc926ded31f39
Michal Burda <michal.burda@osu.cz>
2024-03-11 16:07:51 +0100

    Added paired parametric variant to report_oneway()

--- R/report_oneway.R
+++ R/report_oneway.R
@@ -5,11 +5,16 @@
 #' @importFrom stats kruskal.test
 #' @importFrom rstatix kruskal_effsize
 #' @importFrom rstatix dunn_test
+#' @importFrom rstatix anova_test
+#' @importFrom rstatix pairwise_t_test
 report_oneway <- function(data,
                           var,
                           group,
+                          id = NULL,
                           omit_levels = NULL,
-                          type = c("robust", "parametric", "test"),
+                          type = c("robust", "parametric"),
+                          paired = FALSE,
+                          adjust = p.adjust.methods,
                           boxplot = TRUE,
                           digits = 2,
                           thresh = 0.1) {
@@ -19,6 +24,8 @@ report_oneway <- function(data,
     .must_be_character_scalar(var)
     .must_be_character_scalar(group)
     .must_be_character_vector(omit_levels, null = TRUE)
+    .must_be_flag(paired)
+    .must_be_flag(boxplot)
 
     .must_be_factor(data[[group]], name = "data[[group]]")
 
@@ -28,6 +35,7 @@ report_oneway <- function(data,
     }
 
     res <- list()
+    f <- as.formula(paste(var, '~', group))
 
     if (boxplot == TRUE) {
         res[["boxplot"]] <- ggplot(data) +
@@ -39,41 +47,87 @@ report_oneway <- function(data,
             theme(legend.position = "none")
     }
 
-    f <- as.formula(paste(var, '~', group))
-
     if (type == "robust") {
-        fit <- kruskal.test(formula = f, data = data)
-        eff <- kruskal_effsize(data = data, formula = f)
-        tab <- dunn_test(data = data, formula = f, detailed = TRUE)
-        tab$effect <- abs(tab$statistic) / sqrt(tab$n1 + tab$n2)
-        tab$effect = ifelse(tab$effect < 0.3, "small",
-                            ifelse(tab$effect >= 0.5, "large", "moderate"))
-        tab$statistic <- format_number(tab$statistic, digits = digits)
-        tab$`mean rank diff` = format_number(tab$estimate, digits = digits)
-        tab$p <- format_pvalue(tab$p, thresh = thresh)
-        tab$p.adj <- format_pvalue(tab$p.adj)
-        tab <- tab[, c("group1", "group2", "n1", "n2", "mean rank diff", "p", "p.adj", "effect")]
+        if (paired) {
+            stop("not implemented robust + paired")
 
-        res[["test"]] <- paragraph(
-            "Kruskal-Wallis rank sum test: **",
-            format_pvalue(fit$p.value, varname = "p", thresh = thresh),
-            "** ($\\chi^2$ = ",
-            format_number(fit$statistic, digits = digits),
-            ", df = ",
-            fit$parameter,
-            ")")
-        res[["effect"]] <- paragraph(
-            "Kruskal-Wallis Effect Size: **",
-            paste0(eff$magnitude),
-            "** ($\\eta^2$ = ",
-            format_number(eff$effsize, digits = digits),
-            ", ",
-            format_number(eff$effsize * 100, digits = digits),
-            " % of explained variance)")
-        res[["multicomp_title"]] <- paragraph("Dunn's test:")
-        res[["multicomp"]] <- kable(tab)
+        } else {
+            fit <- kruskal.test(formula = f, data = data)
+            eff <- kruskal_effsize(data = data, formula = f)
+            tab <- dunn_test(data = data, formula = f, detailed = TRUE)
+            tab$effect <- abs(tab$statistic) / sqrt(tab$n1 + tab$n2)
+            tab$effect = ifelse(tab$effect < 0.3, "small",
+                                ifelse(tab$effect >= 0.5, "large", "moderate"))
+            tab$statistic <- format_number(tab$statistic, digits = digits)
+            tab$`mean rank diff` = format_number(tab$estimate, digits = digits)
+            tab$p <- format_pvalue(tab$p, thresh = thresh)
+            tab$p.adj <- format_pvalue(tab$p.adj)
+            tab <- tab[, c("group1", "group2", "n1", "n2", "mean rank diff", "p", "p.adj", "effect")]
+
+            res[["test"]] <- paragraph(
+                "Kruskal-Wallis rank sum test: **",
+                format_pvalue(fit$p.value, varname = "p", thresh = thresh),
+                "** ($\\chi^2$ = ",
+                format_number(fit$statistic, digits = digits),
+                ", df = ",
+                fit$parameter,
+                ")")
+            res[["effect"]] <- paragraph(
+                "Kruskal-Wallis Effect Size: **",
+                paste0(eff$magnitude),
+                "** ($\\eta^2$ = ",
+                format_number(eff$effsize, digits = digits),
+                ", ",
+                format_number(eff$effsize * 100, digits = digits),
+                " % of explained variance)")
+            res[["posthoc_title"]] <- paragraph("Dunn's test:")
+            res[["posthoc"]] <- kable(tab)
+        }
+
+    } else if (type == "parametric") {
+        if (paired) {
+            fit <- anova_test(data = data, dv = !!sym(var), wid = !!sym(id), within = !!sym(group))
+            if (!is.null(fit$ANOVA)) {
+                fit <- fit$ANOVA
+            }
+
+            eff <- cut(fit$ges,
+                       breaks = c(-Inf, 0.01, 0.06, 0.14, Inf),
+                       labels = c("negligible", "small", "moderate", "large"),
+                       right = FALSE)
+            tab <- pairwise_t_test(data = data,
+                                   formula = f,
+                                   paired = TRUE,
+                                   p.adjust.method = adjust)
+            tab$p <- format_pvalue(tab$p, thresh = thresh)
+            tab$p.adj <- format_pvalue(tab$p.adj)
+            tab <- tab[, c("group1", "group2", "n1", "n2", "p", "p.adj")]
+
+            res[["test"]] <- paragraph(
+                "One-way repeated measures ANOVA: **",
+                format_pvalue(fit$p, varname = "p"),
+                "** (F = ",
+                format_number(fit$F),
+                ", df = (",
+                fit$DFn, ", ", fit$DFd,
+                "))\n\n")
+            res[["effect"]] <- paragraph(
+                "Effect size: **",
+                as.character(eff),
+                "** ($\\eta^2$ = ",
+                format_number(fit$ges),
+                ", ",
+                format_number(fit$ges * 100),
+                " % of explained variance)")
+            res[["posthoc_title"]] <- paragraph("Pairwise paired t test:")
+            res[["posthoc"]] <- kable(tab)
+
+        } else {
+            stop("not implemented parametric + !paired")
+        }
+
     } else {
-        stop("not yet implemented")
+        stop("not implemented type = ", type)
     }
 
     report(res)
