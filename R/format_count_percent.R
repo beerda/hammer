b3b792e403cc2833317381f0857d70feae3f64fe
Michal Burda <michal.burda@osu.cz>
2024-04-11 12:13:20 +0200

    Modified format_count_percent to allow non-integer input (e.g. weights)

--- R/format_count_percent.R
+++ R/format_count_percent.R
@@ -3,8 +3,8 @@
 #' `p` is the number of positive cases and `n` is the number of all cases. The result
 #' is a character vector formatted as `"<p> (<p/n> %)"`.
 #'
-#' @param p an integerish vector of positive cases.
-#' @param n an integerish vector of all cases.
+#' @param p a numeric vector of positive cases.
+#' @param n a numeric vector of all cases.
 #' @param ignore_na if `FALSE`, the `NA` values will result into something like `"NA (NA %)"`.
 #' If `TRUE`, such results will be replaced with an empty string. Any other value
 #' will be directly used as a replacement.
@@ -18,12 +18,13 @@ format_count_percent <- function(p,
                                  ignore_na = FALSE,
                                  ...) {
     .must_be_atomic_scalar(ignore_na)
-    .must_be_integerish_vector(p)
-    .must_be_integerish_vector(n)
+    .must_be_numeric_vector(p)
+    .must_be_numeric_vector(n)
 
     na <- is.na(p + n + p / n)
     perc <- format_percent(p / n, ...)
-    res <- paste(p, " (", perc, ")", sep = "")
+    num <- format_number(p, digits = 0)
+    res <- paste(num, " (", perc, ")", sep = "")
 
     if (isTRUE(ignore_na)) {
         ignore_na <- ""
